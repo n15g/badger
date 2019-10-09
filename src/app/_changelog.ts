1b9e6b94e98f6dab144ced8dc6d4732872d44519
“n15g” <nick@nicks-mail.com>
2019-10-09 13:28:48 +1100

    Changelog

--- src/app/_changelog.ts
+++ src/app/_changelog.ts
@@ -1,5 +1,6 @@
 export const Changelog: { [id: string]: string } = {
     "0.1.0": "* Beta/Test release.",
 
-    "0.2.0": "* https://github.com/n15g/badger/issues/4 - Badge icons are now absolute urls instead of image reference keys."
+    "0.2.0": "* https://github.com/n15g/badger/issues/4 - Badge icons are now absolute urls instead of image reference keys.\n" +
+        "* https://github.com/n15g/badger/issues/3 - The search box will now also filter invention badges by invention level."
 };
