6c3bbaea076e8fcfda290c53d8d34ab77b7f96b8
n15g <31093750+n15g@users.noreply.github.com>
2019-10-09 13:30:04 +1100

    Feature/invention level (#8)
    * Fixes #3 - Invention level search
    
    * Changelog

--- src/app/badge/filter-badge-search.pipe.ts
+++ src/app/badge/filter-badge-search.pipe.ts
@@ -1,6 +1,7 @@
 import {Pipe, PipeTransform} from "@angular/core";
 import {IBadge} from "coh-content-db";
 import * as _ from 'lodash';
+import {oc} from "ts-optchain";
 
 @Pipe({
     name: "filterBadgeSearch"
@@ -8,8 +9,11 @@ import * as _ from 'lodash';
 export class FilterBadgeSearchPipe implements PipeTransform {
 
     public transform<T extends IBadge>(badges: T[], query?: string): T[] {
-        return query ? _.filter(badges, (badge) => {
-            return _.some(badge.names, (name) => ("" + name.value).toLowerCase().includes(query.toLowerCase()));
-        }) : badges;
+        if (query == undefined) return badges;
+
+        return _.filter(badges, (badge) =>
+            _.some(badge.names, (name) => oc(name).value().toLowerCase().includes(query.toLowerCase()))
+            || _.some(badge.partials, (partial) => oc(partial).inventionLevel(0).toString() == query)
+        );
     }
 }
