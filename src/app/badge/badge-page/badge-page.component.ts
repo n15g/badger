68261d59b8a3d4a1ca7a4cbb4e052ef3db65c19c
Stuart van Beek <art0rz@protonmail.com>
2020-02-23 20:26:57 -0500

    On badge page, display characters that need the badge (#24)

--- src/app/badge/badge-page/badge-page.component.ts
+++ src/app/badge/badge-page/badge-page.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnInit, ViewEncapsulation} from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
-import {Title} from "@angular/platform-browser";
-import {BadgePartialType, IBadge, IServerGroup} from "coh-content-db";
-import {BadgeNamePipe} from "../badge-name.pipe";
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Title} from '@angular/platform-browser';
+import {BadgePartialType, IBadge, IServerGroup} from 'coh-content-db';
+import { ICharacter } from '../../character/character';
+import { CharacterDbService } from '../../character/character-db.service';
+import {BadgeNamePipe} from '../badge-name.pipe';
 
 @Component({
-    selector: "badge-page",
-    templateUrl: "./badge-page.component.html",
-    styleUrls: ["./badge-page.component.scss"],
+    selector: 'badge-page',
+    templateUrl: './badge-page.component.html',
+    styleUrls: ['./badge-page.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
 export class BadgePageComponent implements OnInit {
@@ -17,16 +19,23 @@ export class BadgePageComponent implements OnInit {
 
     public partialTypes: typeof BadgePartialType = BadgePartialType;
 
+    public characters: ICharacter[] = [];
+
     constructor(private route: ActivatedRoute,
                 private title: Title,
-                private badgeName: BadgeNamePipe) {
+                private badgeName: BadgeNamePipe,
+                private characterDb: CharacterDbService) {
     }
 
     public ngOnInit() {
         this.route.data.subscribe(data => {
-            this.serverGroup = data["serverGroup"];
-            this.badge = data["badge"];
+            this.serverGroup = data.serverGroup;
+            this.badge = data.badge;
             this.title.setTitle(`${this.badgeName.transform(this.badge)} | ${this.serverGroup.name} | Badger`);
         });
+        // console.log(this.badge);
+        this.characterDb.getCharacters().subscribe((characters) =>
+            this.characters = characters.filter(character =>
+                character.badges[this.badge.key] === undefined || character.badges[this.badge.key].owned !== true));
     }
 }
