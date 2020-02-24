1c6ebf30dc57e81a8aa102946c009d7e002cf441
Nicholas Armstrong <nick@nicks-mail.com>
2020-02-25 09:27:47 +1100

    Update to `coh-content-db-homecoming@1.1.11`.

--- src/app/_changelog.ts
+++ src/app/_changelog.ts
@@ -59,5 +59,6 @@ export const Changelog: { [id: string]: string } = {
         '* Update to `coh-content-db-homecoming@1.1.10`.\n',
     '1.1.8':
         '* https://github.com/n15g/badger/pull/24 - On badge page, display characters that need the badge.\n',
-
+    '1.1.9':
+        '* Update to `coh-content-db-homecoming@1.1.11`.\n',
 };
