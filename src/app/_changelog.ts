29635f9bafdbb09fcd5aeefec5ec1354461ac6bc
Nicholas Armstrong <nick@n15g.com>
2021-11-30 08:46:52 +1100

    * Update to `coh-content-db-homecoming@1.3.18`. * 1.3.26 changelog

--- src/app/_changelog.ts
+++ src/app/_changelog.ts
@@ -131,4 +131,6 @@ export const Changelog: { [id: string]: string } = {
         '* Update to `coh-content-db-homecoming@1.3.16`.\n',
     '1.3.25':
         '* Update to `coh-content-db-homecoming@1.3.17`.\n',
+    '1.3.26':
+        '* Update to `coh-content-db-homecoming@1.3.18`.\n',
 };
