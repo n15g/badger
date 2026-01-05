75843143e5872b418ab0a12f971e293b8995f2c0
Nicholas Armstrong <nick@n15g.com>
2026-01-05 23:57:30 +1100

    Another base path test

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 2048
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/badger' : '/',
+    plugins: [react()],
+    build: {
+      chunkSizeWarningLimit: 2048
+    }
   }
 })
