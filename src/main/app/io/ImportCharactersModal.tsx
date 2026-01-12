f612dcf976e5f7ef1330b8f09782260d408856e1
Nicholas Armstrong <nick@n15g.com>
2026-01-12 21:38:25 +1100

    Fixed import stepper

--- src/main/app/io/ImportCharactersModal.tsx
+++ src/main/app/io/ImportCharactersModal.tsx
@@ -1,16 +1,40 @@
-import { FC, useState } from 'react'
+import { FC, useCallback, useState } from 'react'
 import { Box, Button, Modal, ModalClose, ModalDialog, Stack, Step, StepIndicator, Stepper, Typography } from '@mui/joy'
 import SettitleScriptHelpLink from './/SettitleScriptHelpLink.tsx'
 import { Icons } from '../util/Icons.tsx'
 import FileImportResultSummary from './FileImportResultSummary.tsx'
 import { FileImportResult } from './importer.ts'
 import ImportDropzone from './ImportDropzone.tsx'
+import { DefaultColorPalette, VariantKey } from '@mui/joy/styles/types'
 
 const ImportCharactersButton: FC<{ open: boolean, onClose: () => void }>
   = ({ open, onClose }) => {
 
   const [step, setStep] = useState(1)
   const [fileImportResults, setFileImportResults] = useState<FileImportResult[]>()
+
+  const renderStep = useCallback((idx: number, label: string) => {
+    let variant: VariantKey = 'outlined'
+    let color: DefaultColorPalette = 'primary'
+    if (step === idx) {
+      variant = 'solid'
+    } else if (idx < step) {
+      variant = 'solid'
+      color = 'success'
+    }
+    return (
+      <Step
+        indicator={
+          <StepIndicator
+            color={color}
+            variant={variant}>
+            {idx < step ? <Icons.Check/> : idx}
+          </StepIndicator>
+        }>
+        {label}
+      </Step>
+    )
+  }, [step])
 
   return (<>
     {open && (<Modal
@@ -24,17 +48,9 @@ const ImportCharactersButton: FC<{ open: boolean, onClose: () => void }>
             <Typography level="title-lg">Import Characters</Typography>
 
             <Stepper sx={{ m: 2 }}>
-              <Step
-                indicator={
-                  <StepIndicator
-                    color="primary"
-                    variant={step === 1 ? 'soft' : 'solid'}>
-                    {step === 1 ? '1' : <Icons.Check/>}
-                  </StepIndicator>
-                }>
-                Upload Files
-              </Step>
-              <Step>Step 2</Step>
+              {renderStep(1, 'Upload')}
+              {renderStep(2, 'Review Files')}
+              {renderStep(3, 'Review Characters')}
             </Stepper>
 
             {step === 1 && (<>
