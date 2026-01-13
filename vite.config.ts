06bfe949d5e081c8bf698941635eaca56b5bb22e
Nicholas Armstrong <nick@n15g.com>
2026-01-14 02:26:11 +1100

    Fixes #51 - Polyfills for ECMA2023 support.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/badger',
-  plugins: [react()],
+  plugins: [react(), legacy({
+    targets: ['defaults', 'not IE 11'],
+    polyfills: ['es.array.to-sorted'],
+    modernPolyfills: ['es.array.to-sorted'],
+  })],
   build: {
     chunkSizeWarningLimit: 2048
   }
